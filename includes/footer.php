97aee66b76a04cb3602e9d77970eacec05f914ff
alba-mu <albamu2002@gmail.com>
2025-11-04 19:21:47 +0100

    Simplification footer html

--- includes/footer.php
+++ includes/footer.php
@@ -1,23 +1,15 @@
-<footer class="bg-dark text-light mt-5 pt-4 pb-3">
-    <div class="container text-center">
-        <div class="row mb-3">
-            <div class="col">
-                <p class="mb-1 fw-bold">Alba Muñoz Ordoño</p>
-                <p class="mb-1">Curso 2025 / 2026</p>
-                <a href="<?= BASE_URL ?>index.php" class="text-decoration-underline text-white">Inicio</a>
-            </div>
-        </div>
+<footer class="bg-dark text-light mt-5 pt-4 pb-3 w-100 text-center">
 
-        <hr class="border-secondary">
+    <p class="mb-1 fw-bold">Alba Muñoz Ordoño</p>
+    <p class="mb-1">Curso 2025 / 2026</p>
+    <a href="<?= BASE_URL ?>index.php" class="text-decoration-underline text-white">Inicio</a>
 
-        <div class="row">
-            <div class="col">
-                <p class="mb-0 small">
-                    &copy; <?= date('Y'); ?> Mi Sitio Web. Todos los derechos reservados.
-                </p>
-            </div>
-        </div>
-    </div>
+    <hr class="border-secondary mx-auto w-75">
+
+    <p class="mb-0 small">
+        &copy; <?= date('Y'); ?> Mi Sitio Web. Todos los derechos reservados.
+    </p>
+
 </footer>
 
 <!-- Bootstrap JS (bundle con Popper) -->
